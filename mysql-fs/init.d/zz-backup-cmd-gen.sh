e5faa288eab8e3b579c410f72e16e17d058ca548
Motalleb Fallahnezhad <fmotalleb@dornica.net>
2025-11-05 16:38:06 +0330

    fix: typo errors

--- mysql-fs/init.d/zz-backup-cmd-gen.sh
+++ mysql-fs/init.d/zz-backup-cmd-gen.sh
@@ -5,7 +5,7 @@ cmd=(
   "--host='$DB_HOST'"
   "--user='$DB_USER'"
   "--threads=$BACKUP_THREADS"
-  "--outputdir=$BACKUP_NAME"
+  "--outputdir='$BACKUP_NAME'"
   '--clear'
   '--trx-tables=0'
 )
@@ -14,7 +14,7 @@ if [ ! -z "${DB_PASS:-}" ];then
   cmd+=("--password='${DB_PASS}'")
 fi
 if [ ! -z "${DB_NAME:-}" ];then 
-  cmd+=("---database='${DB_NAME}'")
+  cmd+=("--database='${DB_NAME}'")
 fi
 
 BACKUP_COMMAND="${cmd[*]}"
